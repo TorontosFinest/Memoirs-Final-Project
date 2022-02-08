362caef4ad6cc61da878ed7d82bacc6e89d62312
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-09 03:34:49 +0530

    homepage made with tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-
-import { Header } from "./container";
-import { Navbar } from "./components";
-
+import Body from "./components/Body/Body";
+import "./App.css";
 const App = () => (
-  <div>
-    <Navbar />
-    <Header />
+  <div className="mt">
+    <Body />
   </div>
 );
 
---
+++ client/src/components/Body/Body.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./body.css";
+import Form from "../Form/Form";
+export default function Body() {
+  return (
+    <div className="body">
+      <Form />
+    </div>
+  );
+}
