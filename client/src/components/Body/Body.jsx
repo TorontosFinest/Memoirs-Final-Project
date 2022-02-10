cbee810daa5bd3856af91ae32d7618cb2e193e36
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-11 04:03:36 +0530

    change homepage responsive on all devices upto 1920px

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import "./body.css";
-import Form from "../Form/Form";
+// import Form from "../Form/Form";
+import HeaderTitle from "../HeaderTitle/HeaderTitle";
+import RegisterForm from "../RegisterForm/RegisterForm";
 export default function Body() {
   return (
-    <div className="body">
-      <Form />
+    <div className="body lessThan-sm-screens">
+      {/* <Form /> */}
+      <HeaderTitle />
+      <RegisterForm />
     </div>
   );
 }
