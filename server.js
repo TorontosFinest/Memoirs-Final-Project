0746d8d923973542c5f190adb592122fbf06c3dc
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-13 00:28:47 +0530

    database reset scripts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db/db");
 const app = express();
 const PORT = 8080 || 5000;
 
@@ -9,11 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  const data = {
-    first: "Prabhu",
-    second: "Vardhan",
-  };
-  res.json(data);
+  console.log("just get");
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const register = await pool.query(
+      `
+      INSERT INTO users (name,email,password)
+VALUES ($1, $2, $3) RETURNING *;`,
+      [req.body.fullname, req.body.email, req.body.password]
+    );
+    res.json(register);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(PORT, () => {
