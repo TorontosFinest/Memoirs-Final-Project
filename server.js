1574b1c15fa76e54d9be1607853d92ee19610c72
Abdirahman <abdialicanada20@gmail.com>
2022-02-12 11:31:30 -0500

    add cookie sessions

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 8080 || 5000;
+const cookieSession = require("cookie-session");
 
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  cookieSession({
+    name: "SESH",
+    keys: ["key1,", "key2"],
+  })
+);
 
 app.get("/", (req, res) => {
   const data = {
