943e54a13f65c1856edb4d0bc703b3d6d7329e31
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-12 19:06:05 +0530

    modified

---
+++ client/src/components/HeaderTitle/HeaderTitle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function HeaderTitle() {
+  return (
+    <div className="flex flex-col justify-center items-center 2xl:float-left 2xl:ml-20">
+      <h1 className=" mt-20 font-Bungee text-3xl text-white text-center font-bold sm:text-4xl md:text-5xl xl:text-6xl 2xl:mt-64 3xl:text-8xl">
+        Mmemoirs
+      </h1>
+      <h2 className="font-Festive text-sm sm:text-lg md:text-xl xl:text-2xl 3xl:text-5xl text-white text-center font-light ">
+        Nostalgia Re-lived
+      </h2>
+    </div>
+  );
+}
