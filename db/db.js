b6780cd7998dcabf51dc9550bfcd00bf7ecb8991
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-13 03:22:41 +0530

    new changes

--- db/db.js
+++ db/db.js
@@ -1,11 +1,27 @@
-const { Pool } = require("pg");
+require("dotenv").config();
+// other dependencies
+const pg = require("pg");
+// const pool = new Pool({
+//   username: "postgres",
+//   // username: process.env.USERNAME,
+//   password: "admin",
+//   // password: process.env.PASSWORD,
+//   database: "mmemoirs",
+//   // database: process.env.DB_NAME,
+//   port: "5433",
+//   // port: process.env.PORT,
+//   host: "localhost",
+//   // host: process.env.HOST,
+// });
 
-const pool = new Pool({
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.PORT,
-  host: process.env.HOST,
+const connectionString = `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const client = new pg.Client({
+  connectionString: connectionString || "",
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
+client
+  .connect()
+  .then(() => console.log("connected"))
+  .catch((err) => console.error("connection error", err.stack));
 
-module.exports = pool;
+module.exports = client;
