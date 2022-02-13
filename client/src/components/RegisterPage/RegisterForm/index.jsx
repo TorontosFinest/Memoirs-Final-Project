c5643aa3f142b1169743f54a3fd5297528fa09f7
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-14 03:05:29 +0530

    responsive both login and register

--- client/src/components/RegisterPage/RegisterForm/index.jsx
+++ client/src/components/RegisterPage/RegisterForm/index.jsx
@@ -20,9 +20,9 @@ export default function RegisterForm() {
   // jsx
   return (
     <div>
-      <form className="flex flex-col justify-center items-center mt-32 gap-y-2 sm:gap-y-4 xl:gap-y-6 2xl:mt-56 2xl:float-right 2xl:mr-20 2xl:gap-y-10 3xl:float-none 3xl:ml-20 3xl:mt-56">
+      <form className="flex flex-col justify-center items-center mt-20 gap-y-2 sm:gap-y-4 md:mt-72 lg:mt-10 xl:gap-y-6 2xl:mt-56 2xl:float-right 2xl:mr-20 2xl:gap-y-10 3xl:float-none 3xl:ml-20 3xl:mt-42 3xl:gap-y-3 4xl:gap-y-10 4xl:mt-96 ">
         <input
-          className="rounded-xl border-cyan-400 hover:border-dotted bg-transparent text-white w-50 sm:w-64 md:w-80 xl:w-96 3xl:text-3xl"
+          className="rounded-xl border-cyan-400 hover:border-dotted bg-transparent text-white w-50 sm:w-64 md:w-80 xl:w-96 3xl:text-3xl 4xl:text-5xl 4xl:w-3/5 4xl:mt-20 "
           type="text"
           name="name"
           placeholder="Full Name"
@@ -32,7 +32,7 @@ export default function RegisterForm() {
           }}
         />
         <input
-          className="rounded-xl   border-cyan-400 hover:border-dotted bg-transparent text-white   w-50 sm:w-64 md:w-80 xl:w-96 3xl:text-3xl"
+          className="rounded-xl   border-cyan-400 hover:border-dotted bg-transparent text-white   w-50 sm:w-64 md:w-80 xl:w-96 3xl:text-3xl 4xl:text-5xl 4xl:w-3/5"
           type="email"
           name="email"
           placeholder="Email"
@@ -42,7 +42,7 @@ export default function RegisterForm() {
           }}
         />
         <input
-          className="rounded-xl border-cyan-400 hover:border-dotted bg-transparent text-white w-50 sm:w-64 md:w-80 xl:w-96 3xl:text-3xl"
+          className="rounded-xl border-cyan-400 hover:border-dotted bg-transparent text-white w-50 sm:w-64 md:w-80 xl:w-96 3xl:text-3xl 4xl:text-5xl 4xl:w-3/5"
           type="password"
           name="password"
           placeholder="Password"
@@ -51,15 +51,17 @@ export default function RegisterForm() {
             setPassword(event.target.value);
           }}
         />
-        <div className="flex justify-between items-center text-white text-sm  mt-5 mr-20 space-x-2">
-          <p>Already Registered?</p>
-          <Link to="/login">Log-in</Link>
-        </div>
+
+        <p className="text-white 4xl:text-3xl">Already Registered?</p>
+        <Link className="text-cyan-500 underline 4xl:text-4xl" to="/login">
+          Log-in
+        </Link>
+
         <div className="flex justify-center items center">
           <button
             type="submit"
             onClick={registerfrom}
-            className="transition ease-in-out delay-150  bg-rose-500 hover:-translate-y-0 hover:scale-110 hover:bg-indigo-500 duration-300 hover:text-white font-bold p-1 rounded-lg  text-center items-center w-32 space-x-6 shadow-xl shadow-cyan-500/50 sm:w-44 xl:w-56 3xl:text-4xl"
+            className="transition ease-in-out delay-150  bg-rose-500 hover:-translate-y-0 hover:scale-110 hover:bg-indigo-500 duration-300 hover:text-white font-bold p-1 rounded-lg  text-center items-center w-32 space-x-6 shadow-xl shadow-cyan-500/50 sm:w-44 xl:w-56 3xl:text-4xl 4xl:text-5xl 4xl:p-2"
           >
             Register
           </button>
