94769f964b2f27ac7258fcc3d7f90617c741a77f
S550Stang <prabhuvardhanrao94@gmail.com>
2022-02-18 01:00:39 +0530

    rolled back from master

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ const app = express();
 const PORT = 8080 || 5000;
 const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
-const memoriesRoutes = require("./Routes/memories");
 
 require("dotenv").config();
 
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(
   cookieSession({
     name: "SESH",
@@ -20,9 +25,6 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000,
   })
 );
-
-app.use(memoriesRoutes);
-
 
 app.get("/", (req, res) => {
   console.log("just get");
