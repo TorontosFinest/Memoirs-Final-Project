ce0058cca2c5dcbcca48c54b090cbf7743d3d647
Abdirahman <abdialicanada20@gmail.com>
2022-02-18 21:22:41 -0500

    fix bugs

--- client/src/components/DashboardPage/NavbarComponent/index.jsx
+++ client/src/components/DashboardPage/NavbarComponent/index.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className="flex items-center justify-between p-2 w-screen sm:px-4 md:px-6 2xl:px-24">
       <div className="w-screen">
         <h1 className="text-2xl lg:text-3xl 2xl:text-6xl 2xl:mt-1 text-white font-Poppins bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 to-fuchsia-600">
-          Your Memmoirs.
+          Your Memoirs!
         </h1>
       </div>
       <MyDropdown />
